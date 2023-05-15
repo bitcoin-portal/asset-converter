79287492119d39195731ed90ad0c7bd072a5435b
Nhat Dinh <nhatphutho@gmail.com>
2023-05-15 17:15:20 +0900

    wip

--- ios/AssetConvertor.swift
+++ ios/AssetConvertor.swift
@@ -1,4 +1,18 @@
 final class Converter {
-    let assetInteractor = AssetInteractor()
-//    static 
+    static let assetInteractor = AssetInteractor()
+
+    /// Get list of assetIdV2 compound keys.
+    static func getAssetList(provider: Provider) -> [String] {
+        assetInteractor.getAssetList(provider: provider)
+    }
+    
+    /// Get compound key of assetIdV2 if any match.
+    static func convertProviderAssetToAssetIdV2(asset: ProviderAsset, provider: Provider) -> String? {
+        return assetInteractor.convertProviderAssetToAssetIdV2(asset: asset, provider: provider)
+    }
+
+    /// Get provider currency code.
+    static func convertAssetIdV2ToProviderAsset(assetIdV2: String, provider: Provider) -> ProviderAsset? {
+        
+    }
 }
