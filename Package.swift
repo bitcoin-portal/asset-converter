e9eb723344d99090908a41d50e0f0e1f87610c63
Nhat Dinh <nhatphutho@gmail.com>
2023-05-16 17:27:19 +0900

    refactoring

--- Package.swift
+++ Package.swift
@@ -19,7 +19,6 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: "https://github.com/amplitude/Amplitude-iOS", from: "8.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,7 +26,6 @@ let package = Package(
         .target(
             name: "AssetConvertors",
             dependencies: [
-                // .product(name: "Amplitude", package: "Amplitude-iOS"),
             ],
             path: "ios/",
             // exclude: ["android"],
