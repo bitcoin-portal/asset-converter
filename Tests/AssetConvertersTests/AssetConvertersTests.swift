98e17b7466c288b0118894cae0aeec98fe068b05
Nhat Dinh <nhatphutho@gmail.com>
2026-06-04 09:18:39 +0700

    base

--- Tests/AssetConvertersTests/AssetConvertersTests.swift
+++ Tests/AssetConvertersTests/AssetConvertersTests.swift
@@ -25,13 +25,25 @@ final class AssetConvertersTests: XCTestCase {
         XCTAssertEqual(sut.convertProviderAssetToAssetIdV2(asset: "VERSE-ETH", provider: .simplex), nil)
     }
 
+    func testCanConvertBaseProviderAssetToAssetIdV2() {
+        XCTAssertEqual(sut.convertProviderAssetToAssetIdV2(asset: "ETH-BASE", provider: .banxa), "BASE-ETH-ETH")
+        XCTAssertEqual(sut.convertProviderAssetToAssetIdV2(asset: "USDC-BASE", provider: .banxa), "BASE-ERC20-0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913")
+        XCTAssertEqual(sut.convertProviderAssetToAssetIdV2(asset: "ETH_BASE", provider: .moonpay), "BASE-ETH-ETH")
+        XCTAssertEqual(sut.convertProviderAssetToAssetIdV2(asset: "USDC_BASE", provider: .moonpay), "BASE-ERC20-0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913")
+    }
+
     func testCanConvertAssetIdV2ToProviderAsset() {
         XCTAssertEqual(sut.convertAssetIdV2ToProviderAsset(assetIdV2: "ETH-ERC20-0x249cA82617eC3DfB2589c4c17ab7EC9765350a18", provider: .banxa), "VERSE-ETH")
         XCTAssertEqual(sut.convertAssetIdV2ToProviderAsset(assetIdV2: "ETH-ERC20-0x249cA82617eC3DfB2589c4c17ab7EC9765350a18", provider: .moonpay), "VERSE")
         XCTAssertEqual(sut.convertAssetIdV2ToProviderAsset(assetIdV2: "ETH-ERC20-0x249cA82617eC3DfB2589c4c17ab7EC9765350a18", provider: .simplex), nil)
-        
+
         XCTAssertEqual(sut.convertAssetIdV2ToProviderAsset(assetIdV2: "eth-erc20-0x249ca82617ec3dfb2589c4c17ab7ec9765350a18", provider: .banxa), "VERSE-ETH")
         XCTAssertEqual(sut.convertAssetIdV2ToProviderAsset(assetIdV2: "eth-erc20-0x249ca82617ec3dfb2589c4c17ab7ec9765350a18", provider: .moonpay), "VERSE")
         XCTAssertEqual(sut.convertAssetIdV2ToProviderAsset(assetIdV2: "eth-erc20-0x249ca82617ec3dfb2589c4c17ab7ec9765350a18", provider: .simplex), nil)
+    }
+
+    func testCanConvertBaseAssetIdV2ToProviderAsset() {
+        XCTAssertEqual(sut.convertAssetIdV2ToProviderAsset(assetIdV2: "BASE-ETH-ETH", provider: .banxa), "ETH-BASE")
+        XCTAssertEqual(sut.convertAssetIdV2ToProviderAsset(assetIdV2: "BASE-ETH-ETH", provider: .moonpay), "ETH_BASE")
     }
 }
